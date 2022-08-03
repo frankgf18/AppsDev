f9a9b678232ffa9909627de9c53752d3870ba8be
frankgf18 <104527591+frankgf18@users.noreply.github.com>
2022-08-03 17:54:06 -0500

    Progressbar en barras
    Por actualizar algunas funciones

--- app/src/main/java/com/example/appsdev/Home/Animacion/Animacion.kt
+++ app/src/main/java/com/example/appsdev/Home/Animacion/Animacion.kt
@@ -44,7 +44,8 @@ class Animacion : Fragment() {
         ItemsCards(2,"Bottom Layout", R.id.action_animacion_to_bottomPadre),
         ItemsCards(3,"Text Input", R.id.action_animacion_to_textInput),
         ItemsCards(4,"CardView Desplegable", R.id.action_animacion_to_cardDesplegable),
-        ItemsCards(5,"Motion Layout", R.id.action_animacion_to_motionLayout)
+        ItemsCards(5,"Motion Layout", R.id.action_animacion_to_motionLayout),
+        ItemsCards(6,"ProgressBar", R.id.action_animacion_to_progressbarBarra)
     )
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_animacion, container, false)
---
+++ app/src/main/java/com/example/appsdev/Home/Animacion/ProgressBar/ProgressbarBarra.kt
@@ -0,0 +1,16 @@
+package com.example.appsdev.Home.Animacion.ProgressBar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.appsdev.Core.BaseFragment
+import com.example.appsdev.R
+import com.example.appsdev.databinding.FragmentProgressbarBarraBinding
+
+class ProgressbarBarra : BaseFragment<FragmentProgressbarBarraBinding>(FragmentProgressbarBarraBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
